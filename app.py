4837c8e8daf1b932a89794289487085747b45e0c
vintdime <nocom0540@gmail.com>
2026-09-08 22:55:46 +0300

    1

--- app.py
+++ app.py
@@ -1,31 +1,10 @@
-from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
-import httpx
+from flask import Flask, render_template, url_for
 
+app = Flask(__name__)
 
-app = FastAPI()
-
-templates = Jinja2Templates(directory="templates")
-
-GITHUB_API = "https://api.github.com/repos/vintdime/mykpklectures/contents"
-
-
-def get_repo_contents():
-    response = httpx.get(GITHUB_API)
-    response.raise_for_status()
-
-    return response.json()
-
-
-@app.get("/")
-def home(request: Request):
-    return templates.TemplateResponse(
-        request=request,
-        name="index.html"
-    )
+@app.route("/")
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    items = get_repo_contents()
-
-    for item in items:
-        print(item["type"], item["name"])
+    app.run(debug=True)
